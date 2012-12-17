bf234c53dddfc4d87fd671a4352db827ce83e220
Victor Baumann <victor.baumann@gmail.com>
2012-12-17 15:10:43 +0100

    create a blueprint

--- resweb/core.py
+++ resweb/core.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, redirect, request
+from flask import Flask, Blueprint, g, redirect, request
 from pyres import ResQ, failure
 
 from resweb.views import (
@@ -15,44 +15,47 @@ from resweb.views import (
     DelayedTimestamp
 )
 from base64 import b64decode
+
 app = Flask(__name__)
 app.config.from_object('resweb.default_settings')
 app.config.from_envvar('RESWEB_SETTINGS', silent=True)
-@app.before_request
+mod = Blueprint('resweb', __name__)
+
+@mod.before_request
 def before_request():
     """Make sure we are connected to the database each request."""
     g.pyres = ResQ(app.config['RESWEB_HOST'], password=app.config.get('RESWEB_PASSWORD', None))
 
-@app.teardown_request
+@mod.teardown_request
 def teardown_request(exception):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'pyres'):
         g.pyres.close()
 
-@app.route("/")
+@mod.route("/")
 def overview():
     return Overview(g.pyres).render().encode('utf-8')
 
-@app.route("/working/")
+@mod.route("/working/")
 def working():
     return Working(g.pyres).render().encode('utf-8')
 
-@app.route("/queues/")
+@mod.route("/queues/")
 def queues():
     return Queues(g.pyres).render().encode('utf-8')
 
-@app.route('/queues/<queue_id>/')
+@mod.route('/queues/<queue_id>/')
 def queue(queue_id):
     start = int(request.args.get('start', 0))
     return Queue(g.pyres, queue_id, start).render().encode('utf-8')
 
-@app.route('/failed/')
+@mod.route('/failed/')
 def failed():
     start = request.args.get('start', 0)
     start = int(start)
     return Failed(g.pyres, start).render().encode('utf-8')
 
-@app.route('/failed/retry/', methods=["POST"])
+@mod.route('/failed/retry/', methods=["POST"])
 def failed_retry():
     failed_job = request.form['failed_job']
     job = b64decode(failed_job)
@@ -60,14 +63,14 @@ def failed_retry():
     failure.retry(g.pyres, decoded['queue'], job)
     return redirect('/failed/')
 
-@app.route('/failed/delete/', methods=["POST"])
+@mod.route('/failed/delete/', methods=["POST"])
 def failed_delete():
     failed_job = request.form['failed_job']
     job = b64decode(failed_job)
     failure.delete(g.pyres, job)
     return redirect('/failed/')
 
-@app.route('/failed/delete_all/')
+@mod.route('/failed/delete_all/')
 def delete_all_failed():
     #move resque:failed to resque:failed-staging
     g.pyres.redis.rename('resque:failed', 'resque:failed-staging')
@@ -75,7 +78,7 @@ def delete_all_failed():
     return redirect('/failed/')
 
 
-@app.route('/failed/retry_all')
+@mod.route('/failed/retry_all')
 def retry_failed(number=5000):
     failures = failure.all(g.pyres, 0, number)
     for f in failures:
@@ -83,39 +86,41 @@ def retry_failed(number=5000):
         failure.retry(g.pyres, f['queue'], j)
     return redirect('/failed/')
 
-@app.route('/workers/<worker_id>/')
+@mod.route('/workers/<worker_id>/')
 def worker(worker_id):
     return Worker(g.pyres, worker_id).render().encode('utf-8')
 
-@app.route('/workers/')
+@mod.route('/workers/')
 def workers():
     return Workers(g.pyres).render().encode('utf-8')
 
-@app.route('/stats/')
+@mod.route('/stats/')
 def stats_resque():
     return redirect('/stats/resque/')
 
-@app.route('/stats/<key>/')
+@mod.route('/stats/<key>/')
 def stats(key):
     return Stats(g.pyres, key).render().encode('utf-8')
 
-@app.route('/stat/<stat_id>/')
+@mod.route('/stat/<stat_id>/')
 def stat(stat_id):
     return Stat(g.pyres, stat_id).render().encode('utf-8')
 
-@app.route('/delayed/')
+@mod.route('/delayed/')
 def delayed():
     start = request.args.get('start', 0)
     start = int(start)
     return Delayed(g.pyres, start).render().encode('utf-8')
 
-@app.route('/delayed/<timestamp>/')
+@mod.route('/delayed/<timestamp>/')
 def delayed_timestamp(timestamp):
     start = request.args.get('start', 0)
     start = int(start)
     return DelayedTimestamp(g.pyres, timestamp, start).render().encode('utf-8')
 
 def main():
-    app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
+    app.register_blueprint(mod)
+
+    app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'], debug=True)
 if __name__ == "__main__":
     main()
